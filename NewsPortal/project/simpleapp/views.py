37ff4c6f84c99c0680546ca97f42ad7984798233
lb350 <lb3509@gmail.com>
2023-06-18 20:46:01 +0400

    create static

--- NewsPortal/project/simpleapp/views.py
+++ NewsPortal/project/simpleapp/views.py
@@ -5,5 +5,5 @@ from .models import Product
 class ProductsList(ListView):
     model = Product
     ordering = 'name'
-    template_name = 'products.html'
+    template_name = 'default.html'
     context_object_name = 'products'
