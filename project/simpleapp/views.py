36d3e1dc162bc747e80f48b6cb1f2d995e7e70a5
lb350 <lb3509@gmail.com>
2023-06-20 21:43:55 +0400

    add table, date, context in template

--- project/simpleapp/views.py
+++ project/simpleapp/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
 from .models import Product
+from datetime import datetime
 
 
 class ProductsList(ListView):
@@ -10,6 +11,12 @@ class ProductsList(ListView):
     # )
     template_name = 'products.html'
     context_object_name = 'products'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['next_sale'] = "Распродажа в среду!"
+        return context
 
 
 class ProductDetail(DetailView):
